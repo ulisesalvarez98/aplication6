2624845cfa4a767885256060abd23c296869d78c
ulisesalvarez98 <ulisesalvarezcorio3h@gmail.com>
2016-05-01 10:21:03 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,14 +1,14 @@
 // JavaScript Document
 
-$(document).ready(function(e){
+$(document).ready(function (e){
    document.addEventListener ("deviceready",function(){
 		
-	$('#bepp').tap(function(){
-		navigator.notification.beep(1);
+	$('#beep').tap(function(){
+	navigator.notification.beep (3);
 });//tap beep
 
 $('#vibrar').tap(function(){
-	navigator.notifition.vibrate (1000);
+navigator.notification.vibrate (1000);
 });//tap vibrar
 
 },false);//deviceready
